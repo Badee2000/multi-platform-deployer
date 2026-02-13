cb7e735a1417be0bd863a94c0c5a510d4fa5108c
Badee Esmandar <badeealiesmandar@gmail.com>
2026-02-13 17:55:54 +0100

    Bump version to 0.1.1

--- src/multi_platform_deployer/__init__.py
+++ src/multi_platform_deployer/__init__.py
@@ -2,7 +2,7 @@
 Multi-Platform Deployer - A unified Python package for deploying to multiple platforms.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Multi-Platform Deployer Team"
 
 from .main import Deployer
